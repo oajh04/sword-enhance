27399f58d05dc6996d3e1843227dc608c46a7d2c
oajh04 <cloudtubekr@naver.com>
2021-01-26 22:41:52 +0900

    Fail

---
+++ src/Component/Modal/FailEnhanceModal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as S from '../styled/FailEnhanceStyle';
+
+const FailEnhanceModal = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default FailEnhanceModal;
