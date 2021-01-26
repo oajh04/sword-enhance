819dfebb94dd073cf284e0505fa4e8185d86cc05
oajh04 <cloudtubekr@naver.com>
2021-01-26 17:51:11 +0900

    Mainbody

---
+++ src/Component/Main/MainBody.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from '../styled/MainEnhanceStyle';
+
+const MainBody = () => {
+    return (
+        <>
+            <S.EnhanceBody>
+                
+            </S.EnhanceBody>
+        </>
+    )
+}
+
+export default MainBody;
