075902624b0f8ca27e224e5cba27f1d17cb0b4c4
oajh04 <cloudtubekr@naver.com>
2021-01-26 17:51:20 +0900

    Mainfooter

---
+++ src/Component/Main/MainFooter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from '../styled/MainEnhanceStyle';
+
+const MainFooter = () => {
+    return (
+        <>
+            <S.EnhanceFooter>
+                
+            </S.EnhanceFooter>
+        </>
+    )
+}
+
+export default MainFooter;
