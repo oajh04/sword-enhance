29c8990df9bb29d492787c7e65e97fe3bc8b2516
oajh04 <cloudtubekr@naver.com>
2021-01-26 17:51:33 +0900

    mainstyle

---
+++ src/Component/styled/MainEnhanceStyle.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const MainEnhanceBorder = styled.div`
+    width: 100%;
+    height: 100%;
+    min-width: 1400px;
+`
+
+export const EnhanceAll = styled.div`
+    width: 900px;
+    height: 600px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border: 1px solid black;
+`
+
+export const EnhanceHeader = styled.div`
+    height: 100px;
+    display: flex;
+`
+
+export const HeaderRigth = styled.div`
+    width: 300px;
+`
+
+export const HeaderCenter = styled.div`
+    width: 300px;
+    text-align: center;
+`
+
+export const HeaderLeft = styled.div`
+    width: 300px;
+`
+
+export const EnhanceBody = styled.div`
+    height: 400px;
+`
+
+export const EnhanceFooter = styled.div`
+    height: 100px;
+`
+
+// export const Enhance = styled.div`
+// `
