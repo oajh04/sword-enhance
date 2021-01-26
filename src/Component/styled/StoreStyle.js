3b403128ac6209f8c8b53fb5d63809e1af44c139
oajh04 <cloudtubekr@naver.com>
2021-01-26 17:18:20 +0900

    storestyled

---
+++ src/Component/styled/StoreStyle.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const ModalBorder = styled.div`
+    position: absolute;
+    z-index: 99;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100%;
+    background: rgba(0, 0, 0, 0.3);
+    visibility: ${({ visibility }) => visibility};
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+`
+
+export const StoreModal = styled.div`
+    transition: all 0.8s;
+    box-shadow: 8px 10px 30px 0 rgba(0, 0, 0, 0.16);
+    overflow: hidden;
+    width: 350px;
+    border: 1px solid #ffffff;
+    border-radius: 5px;
+    background-color: #ffffff;
+`
+
+export const StoreSort = styled.div`
+    width: 100%;
+    height: 100%;
+`
