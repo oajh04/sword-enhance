1e8ca891dbd7cd1c156e4c4159a684197a2ccc9b
oajh04 <cloudtubekr@naver.com>
2021-01-26 19:18:29 +0900

    body

--- src/Component/Main/MainBody.js
+++ src/Component/Main/MainBody.js
@@ -5,7 +5,17 @@ const MainBody = () => {
     return (
         <>
             <S.EnhanceBody>
-                
+                <S.BodyRight>
+                    판매가격
+                    강화가격
+                </S.BodyRight>
+                <S.BodyCenter>
+                    이미지
+                    강화확률 : 70%
+                </S.BodyCenter>
+                <S.BodyLeft>
+                    아이템 목록
+                </S.BodyLeft>
             </S.EnhanceBody>
         </>
     )
