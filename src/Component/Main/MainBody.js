e0305dc1b8e901df52c62e8fdba93ec9853dd4f9
oajh04 <cloudtubekr@naver.com>
2021-01-27 22:57:23 +0900

    main

--- src/Component/Main/MainBody.js
+++ src/Component/Main/MainBody.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import * as S from '../styled/MainEnhanceStyle';
+import { useSelector, useDispatch } from 'react-redux';
 
 const MainBody = () => {
+    const ReduxResult = useSelector(state => state);
+    const dispatch = useDispatch();
+
+    const myProfile = ReduxResult.MyProfileModule;
+    const Enhance = ReduxResult.EnhanceModule;
+
     return (
         <>
             <S.EnhanceBody>
                 <S.BodySub>
-                    판매가격
-                    강화가격
+                    판매가격 : {Enhance.sellmoney} <br />
+                    강화가격 : {Enhance.probabmoney}
                 </S.BodySub>
                 <S.BodyContent>
-                    이미지
-                    강화확률 : 70%
+                    이미지 <br />
+                    강화확률 : {Enhance.probability * 100}%
                 </S.BodyContent>
                 <S.BodySub>
                     아이템 목록
