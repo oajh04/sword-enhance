b1092aa69eee830e494d446bacb99b16a8f08e1d
oajh04 <cloudtubekr@naver.com>
2021-01-27 22:57:50 +0900

    moduleindex

--- src/Component/modules/index.js
+++ src/Component/modules/index.js
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux';
 import StoreModule from './StoreModule';
 import MyProfileModule from './MyProfileModule'
+import EnhanceModule from './EnhanceModule'
 
-const rootReducer = combineReducers({ StoreModule, MyProfileModule });
+const rootReducer = combineReducers({ 
+    StoreModule, 
+    MyProfileModule,
+    EnhanceModule,
+});
 
 export default rootReducer;
