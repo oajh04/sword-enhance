9ea55b813162cdd698b9882e8ae11f2124d7126f
oajh04 <cloudtubekr@naver.com>
2021-01-27 22:57:28 +0900

    main

--- src/Component/Main/MainFooter.js
+++ src/Component/Main/MainFooter.js
@@ -1,15 +1,56 @@
 import React from 'react';
 import * as S from '../styled/MainEnhanceStyle';
+import { useSelector, useDispatch } from 'react-redux';
+import { enhanceSuccess, enhanceFail } from '../modules/EnhanceModule';
+import { itemEnhance, itemSell } from '../modules/MyProfileModule';
 
-const MainFooter = ({onEnhance}) => {
+const MainFooter = () => {
+    const ReduxResult = useSelector(state => state)
+    const dispatch = useDispatch();
+
+    const myProfile = ReduxResult.MyProfileModule;
+    const Enhance = ReduxResult.EnhanceModule;
+
+    console.log(Enhance)
+
+    const onEnhanceSuccess = () => {
+        dispatch(enhanceSuccess());
+    };
+    const onEnhanceFail = () => {
+        dispatch(enhanceFail());
+    };
+    const onMoneyMius = (e) => {
+        itemEnhance({money: e})
+    }
+    const onMoneyPlus = (e) => {
+        itemSell({money: e})
+    }
+
+    const onEnhanceStart = () => {
+        if(myProfile.money > Enhance.probabmoney){
+            if(Math.random() < Enhance.probability){
+                onEnhanceSuccess()
+            }
+            else{
+                onEnhanceFail()
+            }
+            onMoneyMius(Enhance.probabmoney)
+        }
+    }
+
+    const onItemSell = () => {
+        onMoneyPlus(Enhance.sellmoney);
+        onEnhanceFail()
+    }
+
     return (
         <>
             <S.EnhanceFooter>
                 <S.FooterBox>
-                    판매하기
+                    <div onClick={onItemSell}>판매하기</div>
                 </S.FooterBox>
                 <S.FooterBox>
-                    <div onClick={onEnhance}>강화하기</div>
+                    <div onClick={onEnhanceStart}>강화하기</div>
                 </S.FooterBox>
             </S.EnhanceFooter>
         </>
