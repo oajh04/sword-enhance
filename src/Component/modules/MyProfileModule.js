a3006a7cf6c8f918cbb820246305830b7f7553bb
oajh04 <cloudtubekr@naver.com>
2021-01-27 18:43:55 +0900

    profile module

---
+++ src/Component/modules/MyProfileModule.js
@@ -0,0 +1,29 @@
+const ITEMENHANCE = 'ITEMENHANCE';
+const ITEMSELL = 'ITEMSELL';
+
+export const itemEnhance = (e) => ({
+    type: ITEMENHANCE, 
+    enhanceMoney: e.money, 
+});
+
+export const itemSell = (e) => ({
+    type: ITEMSELL,
+    sellMoney: e.money,
+});
+
+const initialState = {
+    money: 1000000,
+    prevent: 0,
+}
+
+export default function myprofile(state = initialState, action){
+    console.log(action)
+    switch(action.type){
+        case ITEMENHANCE:
+            return state.money - action.enhanceMoney;
+        case ITEMSELL:
+            return state.money + action.sellMoney;
+        default:
+            return state;
+    }
+}
